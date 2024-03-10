62d39a78f27042d27f79f35df238d4bfe05384b1
sinjueun <jueunx0x@naver.com>
2024-03-10 23:31:48 +0900

    스프링 + 뷰 연동

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,18 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
+  },
+  outputDir: "/Users/sinjueun/IdeaProjects/calculator/src/main/resources/static",
+
+  // npm run server 개발 진행시 포트가 다르기 때문에 프록시 설정해줘야 함
+  // target : 백엔드 port가 들어감
+  // changeOrigin -> true로 해야 cros 문제 해결할 수 있음
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
   }
 })
